7e948b295be243d497d988f77b4e0258867b264f
krupa <krupa@KRUPA>
2024-06-21 14:04:59 -0400

    user register using real time database

---
+++ app/src/main/java/com/example/sharebiteapp/ModelData/User.java
@@ -0,0 +1,53 @@
+package com.example.sharebiteapp.ModelData;
+
+public class User {
+    String userID,username,mobilenumber,email,password;
+
+    public User(String userID,String username, String mobilenumber, String email, String password) {
+        this.userID = userID;
+        this.username = username;
+        this.mobilenumber = mobilenumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobilenumber() {
+        return mobilenumber;
+    }
+
+    public void setMobilenumber(String mobilenumber) {
+        this.mobilenumber = mobilenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/sharebiteapp/SignUpActivity.java
+++ app/src/main/java/com/example/sharebiteapp/SignUpActivity.java
@@ -1,18 +1,36 @@
 package com.example.sharebiteapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sharebiteapp.ModelData.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
-    private EditText txtpwd,txtconfpwd;
-    private ImageView eye_password,eye_confpassword;
-
+     EditText txtpwd,txtconfpwd,txtusername,txtmobile,txtemail;
+     ImageView eye_password,eye_confpassword;
+     Button btnsignup;
+     TextView btnlogin;
+     CheckBox chkterms;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
 
     @Override
@@ -20,12 +38,42 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        txtusername = findViewById(R.id.txtusername);
+        txtmobile = findViewById(R.id.txtmobile);
+        txtemail = findViewById(R.id.txtemail);
+        btnsignup = findViewById(R.id.btnsignup);
+        btnlogin = findViewById(R.id.btnlogin);
         txtpwd = findViewById(R.id.txtpwd);
         txtconfpwd = findViewById(R.id.txtconfpwd);
-
         eye_password = findViewById(R.id.eye_password);
         eye_confpassword = findViewById(R.id.eye_confpassword);
+        chkterms = findViewById(R.id.chkterms);
 
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+       //region button on click
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        
+        // endregion
+
+
+        //region icon password click
         eye_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,8 +87,112 @@ public class SignUpActivity extends AppCompatActivity {
                 togglePasswordVisibility(txtconfpwd, eye_confpassword);
             }
         });
+        //endregion
     }
 
+    //region user function
+
+    //validate user
+    public void registerUser()
+    {
+        String username = txtusername.getText().toString().trim();
+        String mobile = txtmobile.getText().toString().trim();
+        String email = txtemail.getText().toString().trim();
+        String password = txtpwd.getText().toString();
+        String confirmPassword = txtconfpwd.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            txtusername.setError("User name is required");
+            txtusername.requestFocus();
+            return ;
+        }
+
+        if (TextUtils.isEmpty(mobile)) {
+            txtmobile.setError("Mobile number is required");
+            txtmobile.requestFocus();
+            return ;
+        }
+        else if(mobile.length() < 10)
+        {
+            txtmobile.setError("Mobile number must have 10 digits");
+            txtmobile.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(email)) {
+            txtemail.setError("Email is required");
+            txtemail.requestFocus();
+            return;
+        }
+        else if (!TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+             txtemail.setError("Invalid email address");
+             txtemail.requestFocus();
+             return ;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            txtpwd.setError("Password is required");
+            txtpwd.requestFocus();
+            return ;
+        }
+        else if(password.length() < 8)
+        {
+            txtpwd.setError("Password should be more than 8 char");
+            txtpwd.requestFocus();
+            return;
+        }
+        else if (!isValidPassword(password)) {
+            txtpwd.setError("Password must contain at least one letter and one digit");
+            txtpwd.requestFocus();
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            txtconfpwd.setError("Passwords do not match");
+            txtconfpwd.requestFocus();
+            return ;
+        }
+        if (!chkterms.isChecked()) {
+            Toast.makeText(SignUpActivity.this, "Please accept terms and conditions", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        User newUser = new User(reference.push().getKey(),username,mobile,email,password);
+        reference.child("users").child(newUser.getUserID()).setValue(newUser)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(SignUpActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUpActivity.this, "registered failed ! please try after sometimes.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+    }
+
+    private boolean isValidPassword(String password) {
+        boolean hasLetter = false;
+        boolean hasDigit = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isLetter(password.charAt(i))) {
+                hasLetter = true;
+            } else if (Character.isDigit(password.charAt(i))) {
+                hasDigit = true;
+            }
+            if (hasLetter && hasDigit) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //toggle to text of password hide and show
     private void togglePasswordVisibility(EditText editText, ImageView imageView) {
         if (editText.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
             // Show Password
@@ -54,4 +206,5 @@ public class SignUpActivity extends AppCompatActivity {
         // Move the cursor to the end of the text
         editText.setSelection(editText.getText().length());
     }
+    //endregion
 }
